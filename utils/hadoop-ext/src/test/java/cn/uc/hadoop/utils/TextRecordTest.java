0656a594ebcf5cc695bcf6c6bf1fc004c0c9c1aa
qus-jiawei <qus-jiawei@gmail.com>
2013-11-13 11:47:36 +0800

    升级版本到1.0.10

--- utils/hadoop-ext/src/test/java/cn/uc/hadoop/utils/TextRecordTest.java
+++ utils/hadoop-ext/src/test/java/cn/uc/hadoop/utils/TextRecordTest.java
@@ -33,6 +33,37 @@ public class TextRecordTest {
 			tr.reset(new Text("哈哈,,ddd,,2323,,"));
 			assertTrue(tr.fieldSize()==4);
 			assertTrue(tr.getRecord().compareTo(new Text("哈哈,,ddd,,2323,,"))==0);
+			
+			
+			TextRecord tr2 = new TextRecord();
+			tr2.setSplit(",");
+			tr2.reset(new Text("a,b,c,d"));
+			assertTrue(tr2.getField(0).equals(new Text("a")));
+			assertTrue(tr2.length==4);
+			
+			tr2.append("abcd");
+			assertTrue(tr2.length==5);
+			assertTrue(tr2.getField(4).equals(new Text("abcd")));
+			
+			assertTrue(tr2.getRecord().equals(new Text("a,b,c,d,abcd")));
+			
+//			TextRecord tr = new TextRecord();//创建对象
+			tr.setSplit("`");//设置分隔符
+			Text temp = new Text("abcd`acd");
+			tr.reset(temp);//将其他的Text存入record进行打散
+			Text t0 = tr.getField(0);//获取第0个Text并使用
+			t0.set("b");
+			assertTrue(tr.getRecord().equals(new Text("b`acd")));
+			Text t1 = tr.getField(1);//获取第1个Text并使用
+			tr.getField(0).set("a");//修改
+			Text returnText = tr.getRecord();//组合为最终的Text
+			assertTrue(returnText.equals(new Text("a`acd")));
+//			tr.reset(other);//处理下一个text
+			
+			
+			assertTrue(tr.find("a")==0);
+			assertTrue(tr.find("acd")==1);
+			assertTrue(tr.find(new Text("acd"))==1);
 		}
 		catch(Exception e){
 			e.printStackTrace();
